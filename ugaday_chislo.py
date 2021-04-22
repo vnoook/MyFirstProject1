84464ecc6bed6d6cc38aefcd09ec5c833ac659d4
vnoook <noook@yandex.ru>
2021-04-22 12:40:51 +0700

    Initial commit

---
+++ ugaday_chislo.py
@@ -0,0 +1,18 @@
+import random
+
+xCh: int = random.randint(1, 100)
+Ug = 0
+
+print("Игра 'Угадай число от 1 до 100'")
+print("Я загадал")
+while xCh != Ug:
+    Ug = int(input("Введите число "))
+    if Ug == 0:
+        print("Вы знаете секрет :-)")
+        break
+    elif Ug < xCh:
+        print("Угадываемое больше Вашего")
+    elif Ug > xCh:
+        print("Угадываемое меньше Вашего")
+else:
+    print("Вы угадали число", xCh)
